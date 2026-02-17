dc9720709f1878c05056c4c0d40df4afeaf0adda
faisalkhalildev <sfaisalshah2024@gmail.com>
2026-02-17 17:18:43 +0500

    fix one bug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(express.json())
+// app.use(express.json())
 connectDB()
 app.use("/api/user" , user)
 app.use("/api/todos", todoRoutes);
